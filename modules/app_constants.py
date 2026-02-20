524d5e1b7459461dc1c89b4235ba33a439907840
USERPROFILE-UNKNOWN <camerondrake001@gmail.com>
2026-02-19 22:02:32 -0600

    v6.15.0
    v6.15.0

--- modules/app_constants.py
+++
@@ -1,9 +0,0 @@
-"""Application-wide constants.
-
-Keep this file as the single source of truth for the current release version.
-"""
-
-APP_NAME = "TradingBot"
-APP_VERSION = "v6.22.1"
-APP_RELEASE = "Regression gates for startup and config safety"
-
